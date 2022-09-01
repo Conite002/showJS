08fcbdb3ab23ebc7109a6c663f96ddf97b2819ff
conite002 <conite.gbodogbe@imsp-uac.org>
2022-09-01 12:22:27 +0200

    clean someone data

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import {Landmark}  from "./src/components/landmark.js";
 var ld = new Landmark({ x : 100, y : 500}, { width : 300, height :  500}, {
                     age : [12, 234, 17, 29],
                     sexe: ["un", "deux", "trois", "quatre"],
-                    nom: [15, 'michel', 'Sarah', 'Paule'],
+                    nom: ['15', 'michel', 'Sarah', 'Paule'],
                     x:"nom"
                 },
-                { x : true, y : true }
             );
--- src/components/landmark.js
+++ src/components/landmark.js
@@ -1,16 +1,22 @@
 
 export class Landmark{
-    constructor(position = { x : 0, y : 0 } , dimension = { width : 0, height : 0 }, prop = {}, display_graduations = { x : false, y : false }){
+    constructor(position = { x : 0, y : 0 } , dimension = { width : 0, height : 0 }, prop = {}, display_graduations = { x : true, y : true }){
         this.position = position;
         this.dimension = dimension;
-        this.x_axis = "";
-        this.y_axis = "";
         this.display_graduations = display_graduations;
+        this.minStep = this.findMinStep(prop);
+        this.y_pas = this.create_y_pas(prop);
+        this.x_pas = this.create_x_pas(prop);
+        this.axis =  this.create_axis(prop);
+    }
+
+    findMinStep(prop){
+        var minStepX , minStepY;
         if(prop.x){ 
             if(typeof prop[prop.x][0] == "string")
-                this.minStepX = this.dimension.width / prop[prop.x].length;
+                minStepX = this.dimension.width / prop[prop.x].length;
             else
-                this.minStepX = 1;
+                minStepX = 1;
         }
         else{
             var key;
@@ -19,15 +25,15 @@ export class Landmark{
                     key = v; 
             });
             if(typeof prop[key][0] == "string")
-                this.minStepX = this.dimension.width / prop[key].length;
+                minStepX = this.dimension.width / prop[key].length;
             else
-                this.minStepX = 1;
+                minStepX = 1;
         }
         if(prop.y ){ 
             if(typeof prop[prop.y][0] == "string")
-                this.minStepY = this.dimension.height / prop[prop.y].length;
+                minStepY = this.dimension.height / prop[prop.y].length;
             else
-                this.minStepY = 1;
+                minStepY = 1;
         }
         else{
             var key;
@@ -36,20 +42,15 @@ export class Landmark{
                     key = v; 
             });
             if(typeof prop[key][0] == "string")
-                this.minStepY = this.dimension.height / prop[key].length;
+                minStepY = this.dimension.height / prop[key].length;
             else
-                this.minStepY = 1;
+                minStepY = 1;
         }
-
-        this.y_pas = [];
-        this.x_pas = [];
-        this.create_y_pas(prop);
-        this.create_x_pas(prop);
-        this.create_axis(prop);
+        return { x : minStepX, y : minStepY }
     }
 
     create_axis(prop){
-        var key_x, key_y, cpt = 0, p = 0.06;
+        var key_x, key_y, cpt = 0, p = 0.06, x_axis, y_axis;
         Object.keys(prop).map((v, index) =>{
             if(index == 0)
                 key_x = v;
@@ -57,8 +58,8 @@ export class Landmark{
                 key_y = v;
         });
 
-        this.x_axis = aya.Polyline([this.position.x, this.position.y, this.position.x + this.dimension.width, this.position.y]);
-        this.y_axis = aya.Polyline([this.position.x, this.position.y, this.position.x, this.position.y - this.dimension.height]);
+        x_axis = aya.Polyline([this.position.x, this.position.y, this.position.x + this.dimension.width, this.position.y]);
+        y_axis = aya.Polyline([this.position.x, this.position.y, this.position.x, this.position.y - this.dimension.height]);
         if(this.display_graduations.x){
             for(var i = 0; i < this.x_pas.length; i++){
                 var pl = aya.Polyline([this.x_pas[i], this.position.y - 3, this.x_pas[i], this.position.y + 3]);
@@ -68,7 +69,7 @@ export class Landmark{
                     var text = aya.Text(this.x_pas[i] - prop[key_x][i].length*5, this.position.y + 15,  prop[key_x][i]);
                 //var pl = aya.Polyline([0, 0, 0, 0]);
                 pl.addChild(text, (p,c)=>{},null, true);
-                this.x_axis.addChild(pl, (p,c) =>{}, null, true);
+                x_axis.addChild(pl, (p,c) =>{}, null, true);
             }
         }
         if(this.display_graduations.y){
@@ -79,10 +80,10 @@ export class Landmark{
                 else
                     var text = aya.Text(this.position.x - Math.floor(prop[key_y][i].length /(2*p)) -15, this.y_pas[i], prop[key_y][i]);
                 pc.addChild(text, null, null, true);
-                this.y_axis.addChild(pc, (p,c) =>{}, null, true);
+                y_axis.addChild(pc, (p,c) =>{}, null, true);
             }
         }
-        for(var m of this.x_axis.children){
+        for(var m of x_axis.children){
             if((m.child.children[0].child.x + m.child.children[0].child.text.length) >= Math.abs(this.x_pas[0] - this.x_pas[1])){
                console.log(m.child.children[0].child);
                 cpt++;
@@ -90,7 +91,7 @@ export class Landmark{
             }
         }
         if(cpt){
-            this.x_axis.children.map(({child}, index) =>{
+            x_axis.children.map(({child}, index) =>{
                 var text = child.children[0].child;
                 if(prop.x)
                     text.x = this.x_pas[index] - prop[prop.x][index].length*7;
@@ -103,27 +104,29 @@ export class Landmark{
             });
         }
 
-        this.x_axis.draw();
-        this.y_axis.draw();
+        x_axis.draw();
+        y_axis.draw();
+
+        return { x : x_axis, y : y_axis };
     }
 
     create_y_pas(prop){
-        var i, y, tmp, key;
+        var i, y, tmp, key, y_pas = [];
         if(prop.y){
             if(typeof prop[prop.y][0] == "string"){
                 for( i = 0; i < this.dimension.height/prop[prop.y].length; i++){
-                    this.y_pas[i] = this.position.y - i*this.minStepY;
+                    y_pas[i] = this.position.y - i*this.minStep.y;
                 }
             }
             else{
                 tmp = this.dimension.height / 2;
-                while(tmp > this.minStepY ){ 
+                while(tmp > this.minStep.y ){ 
                     tmp = tmp / 2;
                 }
                 tmp = tmp*2;
                 y = this.position.y;
                 for(i = 0; i < (this.dimension.height/tmp); i++){
-                    this.y_pas[i] = y;
+                    y_pas[i] = y;
                     y -= tmp;
                 }
             }
@@ -134,36 +137,37 @@ export class Landmark{
                     key = v;
             });
             if(typeof prop[key][0] == "string")
-                for (i = 0; i < this.dimension.height/this.minStepY; i++)
-                    this.y_pas[i] = this.position.y - i*this.minStepY;
+                for (i = 0; i < this.dimension.height/this.minStep.y; i++)
+                    y_pas[i] = this.position.y - i*this.minStep.y;
             else{
                 tmp = this.dimension.height/2;
-                while(tmp > this.minStepY)
+                while(tmp > this.minStep.y)
                     tmp = tmp/2;
                 tmp = tmp*2;
                 for(i = 0; i < (this.dimension.height/tmp); i++){
-                    this.y_pas[i] = this.position.y - i*tmp;
+                    y_pas[i] = this.position.y - i*tmp;
                 }
             }
-        }        
+        }
+
+        return y_pas;
     }
 
     create_x_pas(prop){
-        var i, tmp, key;
-        if(prop.x ){
+        var i, tmp, key, x_pas = [];
+        if( prop.x ){
             if(typeof prop[prop.x][0] == "string")
-                for (i = 0; i < this.dimension.width/this.minStepX; i++)
-                    this.x_pas[i] = this.position.x + i*this.minStepX;
+                for (i = 0; i < this.dimension.width/this.minStep.x; i++)
+                    x_pas[i] = this.position.x + i*this.minStep.x;
             else{
                 tmp = this.dimension.width/2;
-                while(tmp > this.minStepX)
+                while(tmp > this.minStep.x)
                     tmp = tmp/2;
                 tmp = tmp*2;
                 for(i = 0; i < (this.dimension.width/tmp); i++){
-                    this.x_pas[i] = this.position.x + i*tmp;
+                    x_pas[i] = this.position.x + i*tmp;
                 }
-            }
-            
+            } 
         }
         else{
             Object.keys(prop).map((v, index) =>{
@@ -171,18 +175,20 @@ export class Landmark{
                     key = v;
             });
             if(typeof prop[key][0] == "string")
-                for (i = 0; i < this.dimension.width/this.minStepX; i++)
-                    this.x_pas[i] = this.position.x + i*this.minStepX;
+                for (i = 0; i < this.dimension.width/this.minStep.x; i++)
+                    x_pas[i] = this.position.x + i*this.minStep.x;
             else{
                 tmp = this.dimension.width/2;
-                while(tmp > this.minStepX)
+                while(tmp > this.minStep.x)
                     tmp = tmp/2;
                 tmp = tmp*2;
                 for(i = 0; i < (this.dimension.width/tmp); i++){
-                    this.x_pas[i] = this.position.x + i*tmp;
+                    x_pas[i] = this.position.x + i*tmp;
                 }
             }
            
         }
+
+        return x_pas;
     }
 }
